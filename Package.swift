fae8796cb1af2b4430ec4753cd606e7bbd0b5185
Will McGinty <mcgintw@gmail.com>
2018-05-07 20:35:55 -0500

    Remove test API, streamline project structure

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "UrbanVapor",
+    products: [
+        .library(name: "UrbanVapor", targets: ["UrbanVapor"])
+    ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"]),
+        .target(name: "UrbanVapor", dependencies: ["Vapor"], path: "Sources/"),
+        .testTarget(name: "UrbanVaporTests", dependencies: ["Vapor"], path: "Tests/")
     ]
 )
-
