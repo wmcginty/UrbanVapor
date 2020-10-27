103a3bcd0198c930ef1394c5d9a7d074d252c2f8
Will McGinty <mcgintw@gmail.com>
2020-10-26 21:02:48 -0500

    Attempt to fix some compilation issues

--- Sources/Model/Push/Audience.swift
+++ Sources/Model/Push/Audience.swift
@@ -10,20 +10,13 @@ import Foundation
 public struct Audience: Codable {
     
     // MARK: Properties
-    private var audience: [String: AnyEncodable] = [:]
+    private var audience: [String: [String]] = [:]
     
     // MARK: Initializers
-    private init<T: Encodable>(_ dictionary: [String: T]) {
-        audience = dictionary.mapValues(AnyEncodable.init)
+    private init(dictionary: [String: [String]]) {
+        audience = dictionary
     }
-    
-    private init<T: Encodable & Collection>(_ dictionary: [String: T]) where T.Element: Encodable {
-        audience = dictionary.mapValues {
-            guard let first = $0.first, $0.count == 1 else { return AnyEncodable($0) }
-            return AnyEncodable(first)
-        }
-    }
-    
+
     // MARK: Codable
     private struct CodingKeys: CodingKey {
         let stringValue: String
@@ -34,21 +27,29 @@ public struct Audience: Codable {
     }
     
     public func encode(to encoder: Encoder) throws {
-        try audience.encode(to: encoder)
+        for (key, value) in audience {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            
+            if let first = value.first, value.count == 1 {
+                try container.encode(first, forKey: CodingKeys(stringValue: key))
+            } else {
+                try container.encode(value, forKey: CodingKeys(stringValue: key))
+            }
+        }
     }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let dictionary = Dictionary<String, AnyEncodable>(uniqueKeysWithValues: try container.allKeys.map { key in
+        let dictionary = Dictionary<String, [String]>(uniqueKeysWithValues: try container.allKeys.map { key in
             if let singleValue = try? container.decode(String.self, forKey: key) {
-                return (key.stringValue, AnyEncodable(singleValue))
+                return (key.stringValue, [singleValue])
             }
             
             let arrayOfValues = try container.decode([String].self, forKey: key)
-            return (key.stringValue, AnyEncodable(arrayOfValues))
+            return (key.stringValue, arrayOfValues)
         })
         
-        self.init(dictionary)
+        self.init(dictionary: dictionary)
     }
 }
 
@@ -57,51 +58,51 @@ public extension Audience {
     
     // MARK: Channels
     init(iosChannels channels: String...) {
-        self.init(["ios_channel": channels])
+        self.init(dictionary: ["ios_channel": channels])
     }
     
     init(amazonChannels channels: String...) {
-        self.init(["amazon_channel": channels])
+        self.init(dictionary: ["amazon_channel": channels])
     }
     
     init(androidChannels channels: String...) {
-        self.init(["android_channel": channels])
+        self.init(dictionary: ["android_channel": channels])
     }
     
     init(webChannels channels: String...) {
-        self.init(["web_channel": channels])
+        self.init(dictionary: ["web_channel": channels])
     }
 
     init(openChannels channels: String...) {
-        self.init(["open_channel": channels])
+        self.init(dictionary: ["open_channel": channels])
     }
     
     // MARK: Devices
     init(deviceToken token: String) {
-        self.init(["device_token": token])
+        self.init(dictionary: ["device_token": [token]])
     }
 
     init(wnsDevice token: String) {
-        self.init(["wns": token])
+        self.init(dictionary: ["wns": [token]])
     }
     
     // MARK: Segments and Tags
     init(segmentID id: String) {
-        self.init(["segment": id])
+        self.init(dictionary: ["segment": [id]])
     }
     
     init(tag: String, group: String? = nil) {
-        var dict = ["tag": tag]
-        group.map { dict["group"] = $0 }
-        self.init(dict)
+        var dict = ["tag": [tag]]
+        group.map { dict["group"] = [$0] }
+        self.init(dictionary: dict)
     }
     
     init(staticList: String) {
-        self.init(["static_list": staticList])
+        self.init(dictionary: ["static_list": [staticList]])
     }
     
     // MARK: Named Users
     init(namedUser user: String) {
-        self.init(["named_user": user])
+        self.init(dictionary: ["named_user": [user]])
     }
 }
